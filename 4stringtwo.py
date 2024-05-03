cce760f541bb9e871b827ec1547a587cf78d41b7
Kanchan <kanchandhamala1@gmail.com>
2024-05-03 22:28:25 +0545

    initial commet

---
+++ 4stringtwo.py
@@ -0,0 +1,9 @@
+#can use /"...." as well 
+a='he said that,"I love python and I am loving learning python'
+print(a)
+
+b='''Hello guys
+i want to use few statement in different lines
+and experiment so I am using these 3 single
+quote symbol'''
+print(b)
