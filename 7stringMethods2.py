8f78aeee381ec086e73298eaea3ef36759a760eb
Kanchan <kanchandhamala1@gmail.com>
2024-05-04 22:02:12 +0545

    initial commit

---
+++ 7stringMethods2.py
@@ -0,0 +1,16 @@
+c="Welcome to my profile"
+print(c.isalnum())#true if string made of A-Z,a-z,0-9
+print(c.isalpha())#same as isalnumexcept 0-9
+print(c.islower())
+print(c.isprintable())#if \n is present then returns false
+print(c.isspace()) #returns true if white space is present
+print(c.istitle())#returns true if every word is start with capital letter
+print(c.isupper())
+print(c.swapcase())
+print(c.title())
+
+
+
+
+
+
