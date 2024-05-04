86c6e7587c2149c1f6a5e83659b6ce3c0b9d5f37
Kanchan <kanchandhamala1@gmail.com>
2024-05-04 22:24:00 +0545

    initial commit

---
+++ 8IfElse.py
@@ -0,0 +1,8 @@
+a=int(input("Enter your age:"))
+print("Your age is:",a)
+if(a>18):
+    print("You can drive")
+elif(a==18):
+    print("You can drive. You are 18")
+else:
+    print("You can't drive")
