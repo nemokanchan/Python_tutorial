db958289105343a2ba38979d075da67c66422c89
Kanchan <kanchandhamala1@gmail.com>
2024-05-04 21:17:36 +0545

    initial commit

---
+++ 6stringMethods1.py
@@ -0,0 +1,17 @@
+# strings are immutable
+a="!!!!!Kanchan !!!!!!! Kanchan !!!!! "
+print(a)
+print(len(a))
+print(a.upper())
+print(a.lower())
+print(a.rstrip("!"))
+print(a.replace("kanchan","nemo"))
+print(a.count("Kanchan"))
+b="i am SorrY"
+print(b.capitalize())
+print(len(b))
+
+print(b.center(50))
+print(len(b.center(50)))
+print(b.endswith("&&"))
+print(b.index("a"))
