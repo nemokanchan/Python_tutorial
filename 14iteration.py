2fec56bfe17b25956d64628a06dffdc417174bdf
Kanchan <kanchandhamala1@gmail.com>
2024-05-05 21:58:04 +0545

    initial commit

---
+++ 14iteration.py
@@ -0,0 +1,11 @@
+#break and continue
+for i in range(10):
+    if(i==4):
+        print("continue is occured so goes out of iteration")
+        continue
+    if(i==8):
+        print("Occurance of break so goes out of loop")
+        break
+    print(i)
+    
+    
