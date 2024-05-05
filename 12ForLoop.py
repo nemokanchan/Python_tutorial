6c28d26b252c9d599e0c4a6050c24c90695ba186
Kanchan <kanchandhamala1@gmail.com>
2024-05-05 19:53:51 +0545

    initial commit

---
+++ 12ForLoop.py
@@ -0,0 +1,18 @@
+#PRINTS CHARACTER
+name="Kanchan"
+for i in name:
+    print(i)
+    if(i=="n"):
+        print("This is something special")
+#PRINTS LIST ELEMENT
+list=["red","blue","pink","gray"]
+for x in list:
+    print(x)
+    
+#PRINTS NUMBER FROM 1 TO GIVEN NO    
+for n in range(5):
+    print(n)
+ 
+ #CAN APPLY AS C AND C++  AS (START,STOP,DIFF)   
+for m in range(1,12,3):
+    print(m)
