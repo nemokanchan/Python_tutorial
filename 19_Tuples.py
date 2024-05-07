1fa5864d9aa93a27f5963b00ac1d2568995ed99e
Kanchan <kanchandhamala1@gmail.com>
2024-05-07 22:42:17 +0545

    initial commit

---
+++ 19_Tuples.py
@@ -0,0 +1,13 @@
+tup =(2,5,3,"bright")
+print(type(tup),tup)
+print(tup[0])
+print(tup[1])
+print(tup[2])
+print(tup[-1])#lenof(tup)-1
+
+if 3 in tup:
+    print("Yes 3 is present")
+    
+tup2=tup[1:4]
+print(tup2)
+
