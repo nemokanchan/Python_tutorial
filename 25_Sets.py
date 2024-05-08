211e3b6dada7954749f99d2a3f946294454dcfc8
Kanchan <kanchandhamala1@gmail.com>
2024-05-08 22:35:45 +0545

    initial commit

---
+++ 25_Sets.py
@@ -0,0 +1,17 @@
+#Set does not supports repeated values and are similiar to list
+#Sets are unordered collection of data
+s={3,2,5,3}
+print(s)
+
+#They are in curly braces and Set is unchangeable
+info={"army",4,2,"manu",False,5.3}
+print(info)
+
+#Dictionary and set are similar so empty set is not set but is dictionary
+harry={}
+print(type(harry)) #empty dictionary
+
+harry=set()
+print(type(harry)) #empty set
+
+
