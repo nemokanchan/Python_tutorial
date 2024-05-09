4701ee93b1d3f55b266274e517a3f8affb2a7b3d
Kanchan <kanchandhamala1@gmail.com>
2024-05-09 22:02:56 +0545

    initial commit

---
+++ 29_ElseWithForLoop.py
@@ -0,0 +1,9 @@
+for i in range(5):
+    print(i)
+    # if i==2:
+    #     break   Break can be use at middle 
+    # but to know that the loop is commpleted sucessfully we can use else
+    
+else:
+    print("sorry,Out of loop")
+    
