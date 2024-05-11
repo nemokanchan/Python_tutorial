e817889b2230e077f9e564ccc12be57e5f974a64
Kanchan <kanchandhamala1@gmail.com>
2024-05-11 22:54:20 +0545

    initial commit

---
+++ 33shortHandIfElse.py
@@ -0,0 +1,9 @@
+a=33000
+b=30
+
+print("A") if a>b else print("==") if a==b else print("B")
+
+c=1 if a>b else 0
+print(c)
+
+#Its better for use short hand if IF it contains only few lines of code 
