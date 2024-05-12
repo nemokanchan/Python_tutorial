fcaad7cfe6ecbe783b2efc27bfcd60d82df7d822
Kanchan <kanchandhamala1@gmail.com>
2024-05-12 22:57:58 +0545

    initial commit

---
+++ 36_ImportWorks.py
@@ -0,0 +1,12 @@
+#Can import math and use as below
+
+import math
+a=math.floor(4.2343)
+
+result=math.sqrt(25)
+print(result)
+print(a)
+print(".........................................")
+#THE dir function It gives the functons or mrthods inside respective works
+print(dir(math))
+
