fe97cd5339096e89fb5973deb005261b56ea6249
Kanchan <kanchandhamala1@gmail.com>
2024-05-12 22:21:18 +0545

    initial commit

---
+++ 35_VirtualEnviroment.py
@@ -0,0 +1,24 @@
+# #Virtual enviroment helps to create is solated python enviroment where we can install our required packages
+
+# #To create virtual enviroment
+# python -m venv myenv
+
+# #Activate the virtual enviroment (Linux/mac05)
+# source myenvy/bin/activate
+
+# #Activate the virtual enviroment (Windows)
+# myenvy\Scripts\activate.bat 
+
+# #Deactivate the virtual enviroment
+# deactivte
+
+#The "requirements.txt"file
+# This file can be used to easily used to install all the required 
+# packages in a new enviroment.
+# output the list of the installed packages and their versions to a File 
+# pip freeze > requirements.txt 
+
+# Install the packages listed in the requirements.txt file 
+# pip install -r requirements.txt
+
+#NOTE:::: THIS SHOULD BE DONE IN TERMINAL
