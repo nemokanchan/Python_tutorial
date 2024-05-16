4d72782d49a59a9cc26bfc85c5e16c68acc2d355
Kanchan <kanchandhamala1@gmail.com>
2024-05-16 22:32:35 +0545

    initial commit

---
+++ 48_oop.py
@@ -0,0 +1,14 @@
+#OOP in python is used to represent real world concept and entities
+#class is templates or blueprint for creating object
+#Class defines about the properties and methods that the object should have
+
+#Object is an instance of a class and contains its own data
+
+# For example:
+#     If fruit is a class and apple, banana, mango etc are the objects
+#     The commonn properties that object may contain color, taste, cost etc.
+#     The individual obejects has their own properties and the properties are defined on class 
+#     From class objects creates their individual Data.
+
+# So there is no need to make individual data and functions for inividual objects and it makes program simple and easier
+
