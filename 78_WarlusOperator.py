49d46781ddf5fecd1e9ae6214b9ad6da42e78407
Kanchan <kanchandhamala1@gmail.com>
2024-05-26 22:59:57 +0545

    initial commit

---
+++ 78_WarlusOperator.py
@@ -0,0 +1,8 @@
+#Warlus operator(:=) is a new addition in python that allows to assign a value to a variable within an expression
+
+a=True
+print(a:=False)
+
+num=[1,3,4,6,8,9,2]
+while(n:=len(num))>0:
+    print(num.pop)
