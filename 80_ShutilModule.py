44331347ad8da6988469069df56f56775003d7aa
Kanchan <kanchandhamala1@gmail.com>
2024-05-27 23:00:26 +0545

    initial commit

---
+++ 80_ShutilModule.py
@@ -0,0 +1,11 @@
+import shutil
+import os
+
+shutil.copy("file.py","fileCopy.py")#copy the file
+#shutil.copytree("....","....") It copies all the elements and files of folder
+shutil.move(".tutorial/file.file")
+
+os.remove("FolderWantToDelete")
+
+
+
