d47ff643946fb49d8dacfd516774aeae8dbdabc3
Kanchan <kanchandhamala1@gmail.com>
2024-06-02 22:28:01 +0545

    initial commit

---
+++ DrinkWaterRemainder.py
@@ -0,0 +1,7 @@
+import time as t
+from plyer import notification
+while True:
+ 		notification.notify(title = "Please Drink Water Now!!!",message ="paani pee le bhai kaam baad me kar lena",app_icon = "icon.png",timeout= 10)
+        
+        
+t.sleep(3600)
