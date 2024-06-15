79765fb33e984d3e74deaaea35833f2ec8eaa4f9
Kanchan <kanchandhamala1@gmail.com>
2024-06-15 23:01:38 +0545

    initial commit

---
+++ 91_sort.py
@@ -0,0 +1,10 @@
+def sort_numbers(numbers):
+    sorted_numbers = sorted(numbers)
+    return sorted_numbers
+
+# Example usage:
+if __name__ == "__main__":
+    numbers = [5, 2, 9, 1, 5, 6]
+    sorted_numbers = sort_numbers(numbers)
+    print("Original numbers:", numbers)
+    print("Sorted numbers:", sorted_numbers)
