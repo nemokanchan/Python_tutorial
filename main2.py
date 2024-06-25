75a288047d3b82ee5031154cb83bd1e249172492
Kanchan <kanchandhamala1@gmail.com>
2024-06-25 23:26:12 +0545

    initial commit

---
+++ main2.py
@@ -0,0 +1,11 @@
+
+
+def main():
+    # Prompt the user for their name
+    name = input("Enter your name: ")
+    
+    # Print a greeting message
+    print(f"Hello, {name}! Welcome to Python.")
+
+if __name__ == "__main__":
+    main()
